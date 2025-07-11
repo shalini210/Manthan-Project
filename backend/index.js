f1318c657d9bbf6dc4926b06a5486cbea9b8f3a8
shalini <s.rathore43@gmail.com>
2025-07-11 09:53:39 +0530

    xcv

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,1 @@
 const dbconfig = require("./config/dbconfig")
-const demoRouter = require("./routes/demoRouter")
-const express = require("express")
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.get("/",(req,res)=>
-{
-    res.send("use /demo for demo ")
-})
-app.use("/demo",demoRouter)
-app.listen("8080",()=>console.log("server running on localhost:8080"))
